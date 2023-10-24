f73ea7fcf817a035b0b2f3ec7bbca23fedc40a68
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2023-10-23 21:14:50 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx' // Ou o caminho correto para o seu ponto de entrada.
-      }
-    }
-  }
 })

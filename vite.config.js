4854936ebf3b8378e035b31eaaf38209031cbf1b
Willian Batista <72418226+WillianBatista19@users.noreply.github.com>
2023-10-23 21:02:51 -0300

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx' // Ou o caminho correto para o seu ponto de entrada.
+      }
+    }
+  }
 })
